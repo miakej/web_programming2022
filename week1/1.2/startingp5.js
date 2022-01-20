31272040cff7c4ce92750f3d5c6037e4c1272b2d
miakej <mestenson@mcad.edu>
2022-01-20 16:45:29 -0700

    changed shapes when clicked, added effects

--- week1/1.2/startingp5.js
+++ week1/1.2/startingp5.js
@@ -1,19 +1,20 @@
 function setup() {
-    createCanvas(1000,1000);
+    createCanvas(windowWidth, windowHeight);
     background(189, 224, 254);
 }
 
 function draw() {
     let c = color(94, 116, 127);
-
-
     if (mouseIsPressed) {
         fill(c);
         noStroke();
+        blendMode(BURN);
+        square(mouseX, mouseY, 30, 5);
     } else {
         noFill();
         stroke(c);
+        blendMode(DODGE);
+        circle(mouseX, mouseY, 5);
     }
-    square(mouseX, mouseY, 30, 5);
+    
 }
-
