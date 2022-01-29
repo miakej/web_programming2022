7ba3621a5ea2aa08fef6552a7b5902e7717c06a2
miakej <mestenson@mcad.edu>
2022-01-29 16:28:49 -0700

    added space after answer.

--- week2/2.A/main.js
+++ week2/2.A/main.js
@@ -8,4 +8,4 @@ console.log('all trivia questions', questions);
 const randomIdx = Math.round(Math.random() * (questions.length - 1));
 const promptGuess = window.prompt(questions[randomIdx].question);
 
-window.alert('You answered ' + promptGuess + 'The correct answer was ' + questions[randomIdx].answer);
+window.alert('You answered ' + promptGuess + '. The correct answer was ' + questions[randomIdx].answer);
