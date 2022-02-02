23d3093af107f842bcce1b7efb07af0d860e4f79
miakej <mestenson@mcad.edu>
2022-02-01 18:35:12 -0700

    added function notes from class

--- week3/week3_notes/notes3.js
+++ week3/week3_notes/notes3.js
@@ -1,11 +1,12 @@
-// FUNCTION: use functions to execute actions, with various parameters, at a given time. Often to isolate bits of code that are connected, related, or need to occur within a specific sequence.
+// FUNCTION: use functions to execute actions, with various parameters, at a given time. Often to isolate/organize bits of code that are connected, related, or need to occur within a specific sequence. Can be assigned to anything.Think "tupperware" to store code.
+
 // PARAMETER:
 // VALUES: 
 
 // EXAMPLE 1:
-    // function logOutGreeting () {
-    // console.log('hi');   
-    // }
+ /*    function logOutGreeting () {
+    console.log('hi');   
+    } */
 // execute function by using function name:
     // logOutGreeting();
 
@@ -43,6 +44,27 @@ makeSandwich2 = (ingredientArray) => {
 }
 makeSandwich2(['bread', 'hummus', 'cucumber']);
 makeSandwich2(['spinach', 'provolone', 'turkey'])
+// returning data
+function addStuff(itemA, itemB) {
+    console.log("the output is", itemA + itemB)
+}
+addStuff(1, 4);
+
+function addStuff2(itemC, itemD) {
+    return itemC + itemD;
+}
+const sum = addStuff2(2, 5);
+console.log("the sum is ", sum);
+
+function addStuff3(itemE, itemF) {
+    if (!itemE || !itemF) {
+        console.log("no data");
+        return;
+    }
+    const foo = (itemE, itemF) * 17;
+    return foo;
+}
+console.log("the answer is", sum);
 
 // generate random number; 'return' 
 function makeRand (maxNumber) {
@@ -63,14 +85,14 @@ if (checkLegality(MiasAge)){
     console.log('She is legal');
 }
 
-// const myObject = {
-//     x: 200,
-//     y: 400,
-//     getPosition: function () {
-//         console.log('we are ' + this.x + ' from the left and ' + this.y + ' from the top');
-//     }
-// }
-// myObject.getPosition();
+/* const myObject = {
+    x: 200,
+    y: 400,
+    getPosition: function () {
+        console.log('we are ' + this.x + ' from the left and ' + this.y + ' from the top');
+    }
+}
+myObject.getPosition(); */
 
 // 
 const myObject = {
@@ -84,17 +106,29 @@ const myObject = {
 }
 myObject.getPosition();
 
-
+// functions as values in variables, properties, and arguments
+/* const myCar = {
+    wheels: 4,
+    addWheel: function () {
+        console.log("added wheel");
+    }
+}
+myCar.addWheel(); */
+function alertFn() {
+    return "this came from my function";
+}
+window.alert(alertFn);
 
 
 
 // LOOPS
 // for loop with number: for (variable definition; condition; increment/ what happens to the loop variable each time it runs)
-// for (let i = 0; i < 10; i++) {
-//     console.log('this is i', i)
-// }
+/* for (let i = 0; i < 10; i++) {
+    console.log('this is i', i)
+} */
 // for loop with an array 
 const myMovies = ['star wars', 'empire strikes back', 'return of the jedi']
 for (let j = 0; j < myMovies.length; j++) {
     console.log('i like', myMovies[j]);
 }
+
