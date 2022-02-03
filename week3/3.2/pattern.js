a88133ac7d26fe72eb2e704025cb487b00299d1e
miakej <mestenson@mcad.edu>
2022-02-02 19:21:53 -0700

    blue pattern created

---
+++ week3/3.2/pattern.js
@@ -0,0 +1,33 @@
+function setup() {
+    createCanvas(600, 600);
+}
+
+function createTile(originX, originY, primaryColor, strokeColor, secondaryColor, topColor) {
+    translate(originX, originY);
+    fill(primaryColor);
+    stroke(strokeColor);
+    strokeWeight(3);
+    rect(0, 0, 200, 200);
+    fill(secondaryColor);
+    quad(25, 100, 100, 25, 175, 100, 100, 175)
+    fill(topColor);
+    quad(60, 100, 100, 60, 140, 100, 100, 140)
+    line(0, 100, 200, 100);
+    line(100, 0, 100, 200);
+    triangle(0, 0, 0, 60, 60, 0);
+    triangle(0, 200, 0, 140, 60, 200);
+    triangle(140, 200, 200, 200, 200, 140);
+    triangle(140, 0, 200, 0, 200, 60);
+}
+
+function draw() {
+    createTile(0, 0, 'powderblue', 'aliceblue', 'darkcyan', 'darkslategray');
+    createTile(0, 200, 'darkslategray', 'powderblue', 'aliceblue', 'darkcyan');
+    createTile(0, 200, 'powderblue', 'aliceblue', 'darkcyan', 'darkslategray');
+    createTile(200, -400, 'aliceblue', 'darkcyan', 'darkslategray', 'powderblue');
+    createTile(0, 200, 'darkcyan', 'darkslategray', 'powderblue', 'aliceblue');
+    createTile(0, 200, 'aliceblue', 'darkcyan', 'darkslategray', 'powderblue');
+    createTile(200, -400, 'powderblue', 'aliceblue', 'darkcyan', 'darkslategray');
+    createTile(0, 200, 'darkslategray', 'powderblue', 'aliceblue', 'darkcyan');
+    createTile(0, 200, 'powderblue', 'aliceblue', 'darkcyan', 'darkslategray');
+}
