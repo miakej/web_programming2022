66a04ba9981615cb3745172a5a7eb53821c52932
miakej <mestenson@mcad.edu>
2022-02-06 18:07:19 -0600

    quiz game created

---
+++ week3/3.A/quiz.js
@@ -0,0 +1,13 @@
+const questions = [
+    { question: 'red + yellow =', answer: 'orange' },
+    { question: 'yellow + blue =', answer: 'green' },
+    { question: 'blue + red =', answer: 'purple' },
+    { question: '', answer: ''},
+    { question: '', answer: ''},
+]
+console.log('all trivia questions', questions);
+
+const randomIdx = Math.round(Math.random() * (questions.length - 1));
+const promptGuess = window.prompt(questions[randomIdx].question);
+
+window.alert('You answered ' + promptGuess + '. The correct answer was ' + questions[randomIdx].answer);
