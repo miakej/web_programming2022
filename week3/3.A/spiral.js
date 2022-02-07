5a6fb13fd47e607a070fe29d9cab7a2499cc24dc
miakej <mestenson@mcad.edu>
2022-02-06 21:55:42 -0600

    changed stroke color, added notes

--- week3/3.A/spiral.js
+++ week3/3.A/spiral.js
@@ -3,7 +3,7 @@ let r, g, b;
 let spiro = window.prompt("select spiral size");
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight); 
     angleMode(DEGREES);
     r = random(255);
     g = random(255);
@@ -14,19 +14,21 @@ function setup() {
 function makeArm() {
     let alt = Math.round(rotateBy / 360);
     noFill();
-    stroke(0);
+    stroke(255);
     strokeWeight(2);
-    ellipse(spiro, spiro / alt, spiro * alt, spiro * 2);
+    // shapes with custom spiral design size
+    ellipse(spiro, spiro / alt, spiro * alt, spiro * 2); 
     rect(spiro, spiro / alt, spiro * alt, spiro);
 }
 
 function draw() {
-    translate(windowWidth / 2, windowHeight / 2);
+    translate(windowWidth / 2, windowHeight / 2); // keeps centered to screen
     rotate(rotateBy);
     makeArm(rotateBy);
     rotateBy += 8;
-}
+}f
 
 function mousePressed() {
-    background(r, b, g) // refreshes spiro
+    background(r, b, g); // changes background color
+    // spiro refreshes
 }
