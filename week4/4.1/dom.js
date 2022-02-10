87096b56b314226cfd2dba2bf8efb28ff97c53d2
miakej <mestenson@mcad.edu>
2022-02-10 11:30:37 -0700

    updated to change background image

--- week4/4.1/dom.js
+++ week4/4.1/dom.js
@@ -11,8 +11,11 @@ myEmptyDiv.appendChild(heading);
 heading.addEventListener('click', handleHeadingClick);
 function handleHeadingClick() {
     console.log('heading has been clicked');
-    document.body.style.backgroundColor = 'peru';
-    document.body.style.fontSize = '5em';
+    // document.body.style.backgroundColor = 'peru';
+    // updated to change background to image
+    document.body.style.color = 'peru'
+    document.body.style.backgroundImage = 'url(https://media.istockphoto.com/vectors/fast-food-and-seafood-vector-seamless-pattern-vector-id826276628?k=20&m=826276628&s=612x612&w=0&h=PYHXMDqMI9oRa2zt214Gu_NJeQFmhHVWlniUtWDwsLU=)'
+    document.body.style.fontSize = '10em';
 }
 // change cursor to pointer when hovered over heading
 heading.style.cursor = 'pointer';
