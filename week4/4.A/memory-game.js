e15daea772cb3b2e27ff4a2ee7b5e29c23a669bf
miakej <mestenson@mcad.edu>
2022-02-13 12:04:56 -0700

    adjusted card size, added score placeholder text

--- week4/4.A/memory-game.js
+++ week4/4.A/memory-game.js
@@ -1,21 +1,20 @@
-// create a grid of card shapes (view demo);
-// decide number of cards (ie: 20), create 10 shapes; find image for card back (look at demo 1 week 1 grid for snowman for reference on how to bring images into code); do not yet need to place images within card
-
 function setup() {
     createCanvas(900,900);
 }
 let cards = [];
 function draw() {
-    background('peru');
+    background('aliceblue');
     noLoop();
-    fill('aliceblue');
+    noStroke();
+    fill('peru');
     for (let j = 0; j < 4; j++) {
         for (let i = 0; i < 4; i++) {
-            const newCard = {x: i * 200, y: j * 225, width: 150, height: 175}
+            const newCard = {x: i * 175, y: j * 225, width: 150, height: 200}
             rect(newCard.x, newCard.y, newCard.width, newCard.height);
             cards.push();
         }
     }
-
-    console.log('cards', cards);
+    textSize(30);
+    text('score:', 700, 425);
+    text('X matches', 700, 475);
 }
