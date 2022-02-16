04a75782e6a0f6e99edaf7c644a501462106aefe
miakej <mestenson@mcad.edu>
2022-02-15 20:01:20 -0700

    5.1 Demo Exercise

---
+++ week5/5.1/timers.js
@@ -0,0 +1,45 @@
+let blockX = 0;
+let blockY = 0;
+let blockColor = 255;
+let drawTimer;
+const speed = 20;
+const distance = 5;
+
+function setup () {
+    createCanvas(500, 500);
+    background(0);
+}
+
+function drawBlock(x, y, color) {
+    fill(color || 255);
+    rect(x, y, 50, 50);
+}
+
+function keyTyped() {
+    let keyToNumber = Number(key);
+    if (isNaN(keyToNumber)) {
+        return;
+    } else {
+
+    }
+    keyToNumber = map(keyToNumber, 1, 9, 1, 225);
+    console.log('converted number', keyToNumber)
+    blockColor = keyToNumber;
+    // console.log('key to number', keyToNumber);
+}
+
+drawTimer = window.setInterval(() => {
+    if(blockY - 50 <= height) {
+        drawBlock(blockX, blockY, blockColor);
+        blockY += distance;
+    } else {
+        blockY = 0;
+        blockX += 50;
+    }
+
+    if(blockY - 50 > height && blockX - 50 > width) {
+        window.clearInterval(drawTimer);
+        alert('done');
+        console.log('done!');
+    }
+}, speed);
