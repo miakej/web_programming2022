f6fa68776837f1a6584afe21cd61f970380eb17c
miakej <mestenson@mcad.edu>
2022-02-24 17:06:42 -0700

    vue2 exercise

---
+++ week6/6.1/vue.js
@@ -0,0 +1,9 @@
+// new instance of the Vue class
+new Vue({ 
+    el: "#myApp",
+    data: {
+      message: "Hello Doggos!",
+      name: "what is your name?",
+      hobby: "frisbee",
+    }
+  });  
