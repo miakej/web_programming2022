9f6acad915acb6f86f0963be9f113c19be81f931
miakej <mestenson@mcad.edu>
2022-02-27 21:51:52 -0700

    llama vue written

---
+++ week6/6.A/llama.js
@@ -0,0 +1,16 @@
+new Vue({
+    el: "#myLlama",
+    data: {
+        llamaFact: true,
+        llama1: "Llamas are generally about twice the size of alpacas",
+        llama2: "There are more than 7 million llamas and alpacas in South America today",
+        llama3: "Llamas have two wild cousins that have never been domesticated: the vicuña and the guanaco.",
+        llama4: "Llamas are social animals and prefer to live with other llamas or herd animals.",
+        llama5: "Llamas come in a range of solid and spotted colors including black, gray, beige, brown, red, and white.",
+        llama6: "Mama llamas usually only have one baby at a time and llama twins are incredibly rare.",
+        llama7: "Llama poop has almost no odor. Llama farmers refer to llama manure as 'llama beans'",
+        llama8: "A llama's stomach has three compartments.",
+        llama9: "Llamas don't bite. They spit when they're agitated, but that's mostly at each other.",
+        llama10: "Llamas weigh between 280 and 450 pounds and can carry 25 to 30 percent of their body weight."
+    },
+  });
