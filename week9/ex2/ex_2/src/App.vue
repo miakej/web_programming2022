20ff8fec1ab107fbe7e9481f55130dc565c233e5
miakej <mestenson@mcad.edu>
2022-03-25 11:21:48 -0600

    ex2 added to server

---
+++ week9/ex2/ex_2/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+ // javascript here
+ const isWorking = true;
+</script>
+
+<template>
+<!-- html here--> 
+<!-- most important. every component needs at least a template. -->
+<div class="container">
+  <h1>My first compiled Vue app</h1>
+  <p v-if="isWorking">It is working</p>
+</div>
+</template>
+
+<style>
+/* css here */ 
+.container {
+  width: 600px;
+  margin: 200px auto 0;
+  text-align: center;
+  font-size: 18px;
+}
+.container p {
+  font-size: 18px;
+}
+</style>
