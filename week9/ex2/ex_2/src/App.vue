36cf604ef32cdf4796d8ae6afd03d51e11924b9d
miakej <mestenson@mcad.edu>
2022-03-25 16:11:16 -0600

    demo 3 complete

--- week9/ex2/ex_2/src/App.vue
+++ week9/ex2/ex_2/src/App.vue
@@ -1,26 +1,43 @@
 <script setup>
  // javascript here
+ import AnimalRow from './components/AnimalRow.vue';
  const isWorking = true;
+ const animals = [
+      {
+        commonName: "Lama",
+        binomialName: "Lama glama",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Lama",
+        species: "L. glama",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Llama_lying_down.jpg/220px-Llama_lying_down.jpg",
+        id: 1
+        },
+      {
+        commonName: "Alpaca",
+        binomialName: "Vicugna pacos",
+        order: "Artiodactyla",
+        family: "Camelidae",
+        genus: "Vicugna",
+        species: "V. pacos",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/440px-Alpaca_%2831562329701%29.jpg",
+        id: 2
+      },
+    ];
+    
 </script>
 
 <template>
-<!-- html here--> 
-<!-- most important. every component needs at least a template. -->
-<div class="container">
-  <h1>My first compiled Vue app</h1>
-  <p v-if="isWorking">It is working</p>
-</div>
+  <div class="container">
+    <h1>Animals</h1>
+    <AnimalRow v-for="animal in animals" v-bind:key="animal.id" v-bind:animal="animal"/>
+  </div>
 </template>
 
-<style>
-/* css here */ 
-.container {
-  width: 600px;
-  margin: 200px auto 0;
-  text-align: center;
-  font-size: 18px;
-}
-.container p {
-  font-size: 18px;
-}
+<style> 
+  .container {
+    width: 600px;
+    margin: 200px auto 0;
+    font-family: Impact;
+  }
 </style>
---
+++ week9/ex2/ex_2/src/components/AnimalRow.vue
@@ -0,0 +1,34 @@
+<script setup>
+    const props = defineProps(['animal']);
+    console.log(props.animal);
+</script>
+
+<template>
+  <div class="animal-row">
+    <h5>{{props.animal.name}}</h5>
+    <img v-bind:src="props.animal.image" v-bind:alt="props.animal.commonName"/>
+    <p>{{props.animal.caption}}</p>
+  </div>
+</template>
+
+<style scoped>
+h5 {
+  text-align: center;
+  font-size: 18px;
+}
+p {
+  font-size: 18px;
+  text-align: center;
+}
+.animal-row {
+    font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+    border: 1px solid black;
+    margin: 10px;
+    border-radius: 5px;
+}
+.animal-row img{
+  max-width: 300px;
+  display: block;
+  margin: 5px auto 0;
+}
+</style>
