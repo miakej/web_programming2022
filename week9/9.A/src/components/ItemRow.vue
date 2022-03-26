2ec2d3d04be2ab2eff0a1781b38c10c5956e98e4
miakej <mestenson@mcad.edu>
2022-03-26 15:47:54 -0600

    delete functioning

--- week9/9.A/src/components/ItemRow.vue
+++ week9/9.A/src/components/ItemRow.vue
@@ -1,11 +1,11 @@
 <script setup>
-    const props = defineProps (["album"]);
+    const props = defineProps (["album", "index"]);
     console.log('the album', props.album); 
 </script>
 
 <template>
     <div class="tbody">
-        <div class="tr">
+        <div class="tr" v-bind:class="{tr: true, odd: index % 2 !== 0, }">
             <div class="td album-cover">
                 <img :title="album.album" :src="album.cover" :alt="album.artist + ' ' + album.album + ' cover art'"></div>
             <div class="td album-name">{{album.album}}</div>
@@ -13,7 +13,7 @@
             <div class="td album-year">{{album.year}}</div>
             <div class="td album-owned">{{album.owned}}</div>
             <div class="td cellActions">
-                <button type="button" v-on:click="$emit('deleteRecord', item)">Delete</button> 
+                <button type="button" v-on:click="$emit('deleteAlbum', album)">Delete</button> 
             </div>
         </div>
     </div>
