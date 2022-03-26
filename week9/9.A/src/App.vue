dbf30897600e4a18150759795b0261bef396f83f
miakej <mestenson@mcad.edu>
2022-03-26 15:48:03 -0600

    delete functioning

--- week9/9.A/src/App.vue
+++ week9/9.A/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
      import { reactive } from 'vue';
+     import ItemRow from "./components/ItemRow.vue"
      
     const vinyls =  [
             {   
@@ -8,31 +9,36 @@
                 album: "What's Going On",
                 year: "1971",
                 owned: "yes",
-            }, {  
+                id: 1,
+   }, {  
                 cover: "src/covers/joni-mitchell.jpg", 
                 artist: "Joni Mitchell",
                 album: "Blue",
                 year: "1971",
                 owned: "yes",
-            }, {   
+                id: 2,
+   }, {   
                 cover: "src/covers/fleetwood-mac.jpg",
                 artist: "Fleetwood Mac",
                 album: "Rumors",
                 year: "1977",
                 owned: "yes",
-            }, {   
+                id: 3,
+   }, {   
                 cover: "src/covers/bob-dylan.jpg",
                 artist: "Bob Dylan",
                 album: "Blood on the Tracks",
                 year: "1975",
                 owned: "yes",
-            }, {   
+                id: 4,
+   }, {   
                 cover: "src/covers/rolling-stones.jpg",
                 artist: "The Rolling Stones",
                 album: "Exile on Main Street",
                 year: "1972",
                 owned: "not yet",
-            }
+                id: 5,
+}
         ];
     const newVinylObj = {
         cover: " ",
@@ -42,6 +48,29 @@
         owned: ' ',
         };
     const state = reactive({vinyls ,newVinylObj});
+
+    function addNewVinyl () {
+        state.vinyls.push({
+            cover: state.newVinylObj.cover,
+            artist: state.newVinylObj.artist,
+            album: state.newVinylObj.album,
+            year: state.newVinylObj.year,
+            owned: state.newVinylObj.owned,
+            id: state.vinyls.length + 1,
+        });
+        state.newVinylObj.cover = "";
+        state.newVinylObj.artist = "";
+        state.newVinylObj.album = "";
+        state.newVinylObj.year = "";
+        state.newVinylObj.owned = false;
+    }
+
+    function handleDelete (albumToDelete) {
+        console.log(albumToDelete.id, albumToDelete.artist);
+        state.vinyls = state.vinyls.filter((albumToCheck) => {
+            return albumToDelete !== albumToCheck;
+        });
+    }
 </script>
 
 <template>
@@ -49,42 +78,50 @@
   <h1>1970's Vinyl Collection</h1>
      <div id="vinylApp">
         <div class="table">
-            <div class="collectionRow header">
-                <div class="cell cover-image">Cover</div>
-                <div class="cell album-name">Album</div>
-                <div class="cell album-artist">Artist</div>
-                <div class="cell album-year">Year</div>
-                <div class="cell owned">Owned</div>
-                <div class="cell actions">Delete</div>
+            <div class="thead">
+                <div class="th">Cover</div>
+                <div class="th">Album</div>
+                <div class="th">Artist</div>
+                <div class="th">Year</div>
+                <div class="th">Owned</div>
+                <div class="th">Delete</div>
             </div>
-
         </div>
-        <form v-on:submit.prevent="submitHandler" class="new-console-form">
-            <fieldset>
-                <legend>Add a vinyl to the collection</legend>
-                <div>
-                    <label for="cover">Cover Art (URL)</label>
-                    <input id="cover" type="text" v-model="newVinylObj.cover">
-                </div>
-                <div>
-                    <label for="album">Album</label>
-                    <input id="album" type="text" v-model="newVinylObj.album">
-                </div>
-                <div>
-                    <label for="artist">Artist</label>
-                    <input id="artist" type="text" v-model="newVinylObj.artist">
-                </div>
-                <div>
-                    <label for="year">Year</label>
-                    <input id="year" type="text" v-model="newVinylObj.year">
-                </div>
-                <div>
-                    <label for="owned">Owned?</label>
-                    <input id="owned" type="text" v-model="newVinylObj.owned">
-                </div>
-                <button type="submit">Submit</button>
-            </fieldset>
-        </form>
+        <ItemRow 
+            v-for="(vinyl, index) in state.vinyls" 
+            v-bind:key="index" 
+            v-bind:album="vinyl" 
+            v-bind:index="index"
+            v-on:delete-album="handleDelete"
+        />
+        <div class="add-vinyl-form">
+            <form v-on:submit.prevent="submitHandler" class="new-vinyl-form">
+                <fieldset>
+                    <legend>Add a vinyl to the collection</legend>
+                    <div>
+                        <label for="cover">Cover Art (URL)</label>
+                        <input id="cover" type="text" v-model="newVinylObj.cover">
+                    </div>
+                    <div>
+                        <label for="album">Album</label>
+                        <input id="album" type="text" v-model="newVinylObj.album">
+                    </div>
+                    <div>
+                        <label for="artist">Artist</label>
+                        <input id="artist" type="text" v-model="newVinylObj.artist">
+                    </div>
+                    <div>
+                        <label for="year">Year</label>
+                        <input id="year" type="text" v-model="newVinylObj.year">
+                    </div>
+                    <div>
+                        <label for="owned">Owned?</label>
+                        <input id="owned" type="text" v-model="newVinylObj.owned">
+                    </div>
+                    <button type="submit">Submit</button>
+                </fieldset>
+            </form>
+        </div>
     </div>
   </div>
 </template>
@@ -94,31 +131,34 @@ body {
     background: #93E1D8; 
 }
 
-table, thead, th, tbody, tr, td {
+.table, .thead, .th, .tbody, .tr, .td {
+    display:flex;
     margin: 0 auto;
     text-align: center;
-    font-family: 'Calistoga', cursive;
+    font-family: 'Calistoga', sans-serif;
     padding: 1rem;
 }
 
-td {
-    width: 200px;
+.td {
+    justify-content: space-between;
+    align-items: center;
+    min-width: 150px;
 }
 
 h1 {
-    font-family: 'Monoton', cursive;
+    font-family: 'Monoton', sans-serif;
     font-size: 4em;
     text-align: center;
     color: #114B5F;
 }
 
-th {
+.th {
     font-size: 2em;
     background-color: white;
     color: #EEC643; 
 }
 
-td {
+.td {
     font-size: 1.5em;
 }
 
@@ -126,7 +166,7 @@ img {
     max-width: 200px;
 }
 
-tr:hover {
+.tr:hover {
     
     background: #EEC643;
     color: black;
@@ -160,7 +200,7 @@ tr:hover {
 legend {
     background: white;
     color:#EEC643;
-    font-family: 'Calistoga', cursive;
+    font-family: 'Calistoga', sans-serif;
     padding: 10px;
     border-radius: 10px;
 }
