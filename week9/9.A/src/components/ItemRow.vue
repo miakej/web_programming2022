eaf729b78ad85447ffe2aad639e6e81be293f64d
miakej <mestenson@mcad.edu>
2022-03-26 15:07:07 -0600

    functions added (but not working)

--- week9/9.A/src/components/ItemRow.vue
+++ week9/9.A/src/components/ItemRow.vue
@@ -1,19 +1,20 @@
 <script setup>
-    defineProps (["album"]);
-    console.log('the album', album); 
+    const props = defineProps (["album"]);
+    console.log('the album', props.album); 
 </script>
 
 <template>
-
-    <div class="collectionRow">
-        <div class="cell cover-image">
-            <img :title="album.album" :src="album.cover" :alt="album.artist + ' ' + album.album + ' cover art'"></div>
-        <div class="cell album-name">{{album.album}}</div>
-        <div class="cell album-artist">{{album.artist}}</div>
-        <div class="cell album-year">{{album.year}}</div>
-        <div class="cell owned">{{album.owned}}</div>
-        <div class="cell actions">
-            <button type="button" v-on:click="deleteItem(album)">Delete</button> 
+    <div class="tbody">
+        <div class="tr">
+            <div class="td album-cover">
+                <img :title="album.album" :src="album.cover" :alt="album.artist + ' ' + album.album + ' cover art'"></div>
+            <div class="td album-name">{{album.album}}</div>
+            <div class="td artist-name">{{album.artist}}</div>
+            <div class="td album-year">{{album.year}}</div>
+            <div class="td album-owned">{{album.owned}}</div>
+            <div class="td cellActions">
+                <button type="button" v-on:click="$emit('deleteRecord', item)">Delete</button> 
+            </div>
         </div>
     </div>
 </template>
