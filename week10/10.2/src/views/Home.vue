82f933e84d587f1a4ad7191dae8d0d979eae58df
miakej <mestenson@mcad.edu>
2022-03-31 17:57:01 -0600

    10.2 demo finished

---
+++ week10/10.2/src/views/Home.vue
@@ -0,0 +1,41 @@
+<script setup>
+    import MusicList from "../data/music-list.json"
+    console.log("Music List", MusicList);
+</script>
+
+<template>
+    <div class="song-container">
+        <div class="song-list-item" v-for="(song, index) in MusicList" :key="index">
+            <RouterLink :to="'/song/' + song.slug">{{song.title}}</RouterLink>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+  .song-container {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+
+  .song-list-item {
+    width: auto;
+    padding: 10px;
+    margin: 10px;
+    border: 1px solid black;
+    border-radius: 4px;
+    background: white;
+  }
+  .song-list-item:hover {
+    background: black;
+  }
+  .song-list-item a {
+    color: black;
+  }
+  
+  .song-list-item:hover a{
+    color:white;
+  }
+</style>
