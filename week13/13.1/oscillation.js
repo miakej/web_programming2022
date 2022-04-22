36c7b07cd1f91e702cf7efdb09af5f5c57180796
miakej <mestenson@mcad.edu>
2022-04-22 09:01:43 -0600

    tinkering with size and speed

--- week13/13.1/oscillation.js
+++ week13/13.1/oscillation.js
@@ -5,7 +5,7 @@ var slider;
 function setup() {
     createCanvas(windowWidth, 1000, WEBGL);
     rectMode(CENTER);
-    slider = createSlider(0, 10, 5);
+    slider = createSlider(1, 10, 3);
 }
 
 function draw() {
@@ -13,21 +13,22 @@ function draw() {
     background('pink');
     
     a = a + (slider.value()/100);
-    s = sin(a) * 2; 
+    s = sin(a) * 5; 
 
     rotateY(frameCount * 0.01);
-    for (let j = 0; j < 8; j++) {
+    for (let j = 0; j < 100; j++) {
         push();
-        for (let i = 0; i < 80; i++) {
+        for (let i = 0; i < 100; i++) {
           translate(
-            sin(frameCount * 0.01 + j) * 100,
-            sin(frameCount * 0.01 + j) * 100,
+            sin(frameCount * a + j) * 100,
+            sin(frameCount * a + j) * 100,
             i * 0.1
           );
           rotateZ(frameCount * 0.002);
           push();
+          fill('pink');
           stroke('purple');
-          ellipsoid(slider.value()*5);
+          ellipsoid(slider.value()*s);
           pop();
         }
         pop();
