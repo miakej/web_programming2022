c8cc990022d4686abb7bfda18bb01f70211f24e2
miakej <mestenson@mcad.edu>
2022-04-21 22:28:32 -0600

    sketch finalized

--- week13/13.1/oscillation.js
+++ week13/13.1/oscillation.js
@@ -3,7 +3,7 @@ let s = 0.0;
 var slider;
 
 function setup() {
-    createCanvas(710, 400, WEBGL);
+    createCanvas(windowWidth, 1000, WEBGL);
     rectMode(CENTER);
     slider = createSlider(0, 10, 5);
 }
@@ -12,53 +12,22 @@ function draw() {
 
     background('pink');
     
-
-    // set oscil speed with sin of a
-    a = a + (slider.value()/100); // adjust faster/slower here
-    s = sin(a) * 2; // dot 2 grows to twice size
-
-    // translate dot 1 to center, scale with s
-
-    // translate (width/2, height/2);
-    // scale(s);
-    // noFill();
-    // stroke('aliceblue');
-    // ellipse(0,0,50,50);
-
-    // // translate dot 2 from left to right
-
-    // translate(mouseY);
-    // stroke('red');
-    // scale(s);
-    // ellipse(0,0,50,50);
-
-
-    // // translate dot 3 relative to dot 2
-
-    // translate(mouseX);
-    // stroke(0);
-    // scale(s);
-    // ellipse(0,0,50,50);
-
-    // // translate dot 4 relative to dot 3
-
-    // translate(mouseY);
-    // stroke('blue');
-    // scale(s);
-    // ellipse(0, 0, 50, 50);
+    a = a + (slider.value()/100);
+    s = sin(a) * 2; 
 
     rotateY(frameCount * 0.01);
-    for (let j = 0; j < 5; j++) {
+    for (let j = 0; j < 8; j++) {
         push();
         for (let i = 0; i < 80; i++) {
           translate(
-            sin(frameCount * 0.001 + j) * 100,
-            sin(frameCount * 0.001 + j) * 100,
+            sin(frameCount * 0.01 + j) * 100,
+            sin(frameCount * 0.01 + j) * 100,
             i * 0.1
           );
           rotateZ(frameCount * 0.002);
           push();
-          sphere(8, 6, 4);
+          stroke('purple');
+          ellipsoid(slider.value()*5);
           pop();
         }
         pop();
